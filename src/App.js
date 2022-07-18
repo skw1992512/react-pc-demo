a6bafc5ec0b2ce47a403be7e0bbaabebf5000189
skw1992 <470555883@qq.com>
2022-07-18 16:59:30 +0800

    lesson1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import LayOut from "./pages/LayOut";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayOut />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ src/pages/LayOut/index.js
@@ -0,0 +1,5 @@
+function LayOut() {
+  return <div>layout</div>;
+}
+
+export default LayOut;
